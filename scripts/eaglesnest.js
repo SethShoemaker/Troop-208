b291cef9e4bd8fbf20b334bf2e1ef02b1661a59d
Seth Shoemaker <Shoemakerseth2003@gmail.com>
2021-12-25 12:18:46 -0500

    fix js

--- scripts/eaglesnest.js
+++ scripts/eaglesnest.js
@@ -1,98 +1,96 @@
-$( document ).ready(function() {
-    function sortNew() 
-    {
-        var eaglesList = $('#eaglesList');
-        eaglesList.find('.eaglesItems').sort(function(a, b) 
-        {   
-            var x = $(a).attr("data-order"); 			
-            var y = $(b).attr("data-order"); 			
-            if (x > y) 
-            {
-                return -1;
-            } 			
-            if (x < y) 
-            {
-                return 1;
-            } 			
-            return 0; 				
-        }
-        ).appendTo(eaglesList);	  
-    }
-    function sortOld() 
-    {
-        var eaglesList = $('#eaglesList');
-        eaglesList.find('.eaglesItems').sort(function(a, b) 
-        {   
-            var x = $(a).attr("data-order"); 			
-            var y = $(b).attr("data-order"); 			
-            if (x < y) 
-            {
-                return -1;
-            } 			
-            if (x > y) 
-            {
-                return 1;
-            } 			
-            return 0; 				
-        }
-        ).appendTo(eaglesList);	  
-    }
-    function nameAZ() 
-    {
-        var eaglesList = $('#eaglesList');
-        eaglesList.find('.eaglesItems').sort(function(a, b) 
-        {   
-            var x = $(a).attr("data-lastName"); 			
-            var y = $(b).attr("data-lastName"); 			
-            if (x < y) 
-            {
-                return -1;
-            } 			
-            if (x > y) 
-            {
-                return 1;
-            } 			
-            return 0; 				
-        }
-        ).appendTo(eaglesList);	  
-    }
-    function nameZA() 
-    {
-        var eaglesList = $('#eaglesList');
-        eaglesList.find('.eaglesItems').sort(function(a, b) 
-        {   
-            var x = $(a).attr("data-lastName"); 			
-            var y = $(b).attr("data-lastName"); 			
-            if (x > y) 
-            {
-                return -1;
-            } 			
-            if (x < y) 
-            {
-                return 1;
-            } 			
-            return 0; 				
-        }
-        ).appendTo(eaglesList);	  
-    }
-    function test(selectedValue){
-        if(selectedValue === 'sortOld')
+function sortNew() 
+{
+    var eaglesList = $('#eaglesList');
+    eaglesList.find('.eaglesItems').sort(function(a, b) 
+    {   
+        var x = $(a).attr("data-order"); 			
+        var y = $(b).attr("data-order"); 			
+        if (x > y) 
         {
-            sortOld();
-        }
-        else if (selectedValue === 'sortNew'){
-            sortNew();
-        }
-        else if(selectedValue === 'nameAZ') {
-            nameAZ();
-        }
-        else{
-            nameZA();
-        }
+            return -1;
+        } 			
+        if (x < y) 
+        {
+            return 1;
+        } 			
+        return 0; 				
     }
-    $('.eaglesContent').hide();
-    $('.eaglesTitle').click(function(){
-        $(this).find('img').toggleClass('expandActive');
-        $(this).next('.eaglesContent').slideToggle();
-    })
+    ).appendTo(eaglesList);	  
+}
+function sortOld() 
+{
+    var eaglesList = $('#eaglesList');
+    eaglesList.find('.eaglesItems').sort(function(a, b) 
+    {   
+        var x = $(a).attr("data-order"); 			
+        var y = $(b).attr("data-order"); 			
+        if (x < y) 
+        {
+            return -1;
+        } 			
+        if (x > y) 
+        {
+            return 1;
+        } 			
+        return 0; 				
+    }
+    ).appendTo(eaglesList);	  
+}
+function nameAZ() 
+{
+    var eaglesList = $('#eaglesList');
+    eaglesList.find('.eaglesItems').sort(function(a, b) 
+    {   
+        var x = $(a).attr("data-lastName"); 			
+        var y = $(b).attr("data-lastName"); 			
+        if (x < y) 
+        {
+            return -1;
+        } 			
+        if (x > y) 
+        {
+            return 1;
+        } 			
+        return 0; 				
+    }
+    ).appendTo(eaglesList);	  
+}
+function nameZA() 
+{
+    var eaglesList = $('#eaglesList');
+    eaglesList.find('.eaglesItems').sort(function(a, b) 
+    {   
+        var x = $(a).attr("data-lastName"); 			
+        var y = $(b).attr("data-lastName"); 			
+        if (x > y) 
+        {
+            return -1;
+        } 			
+        if (x < y) 
+        {
+            return 1;
+        } 			
+        return 0; 				
+    }
+    ).appendTo(eaglesList);	  
+}
+function test(selectedValue){
+    if(selectedValue === 'sortOld')
+    {
+        sortOld();
+    }
+    else if (selectedValue === 'sortNew'){
+        sortNew();
+    }
+    else if(selectedValue === 'nameAZ') {
+        nameAZ();
+    }
+    else{
+        nameZA();
+    }
+}
+$('.eaglesContent').hide();
+$('.eaglesTitle').click(function(){
+    $(this).find('img').toggleClass('expandActive');
+    $(this).next('.eaglesContent').slideToggle();
 })
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,7 @@
-$( document ).ready(function() {
-
 function openMobileMenu(){
     document.getElementById('mobileMenuContainer').style.transform='translateX(0)';
 }
 function closeMobileMenu(){
     document.getElementById('mobileMenuContainer').style.transform='translateX(-150vw)';
 }
-})
 
