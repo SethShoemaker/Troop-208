a7ffc0275fb3434d4696bf4f4446c274855dfa26
Seth Shoemaker <Shoemakerseth2003@gmail.com>
2022-01-20 22:59:43 -0500

    added docuemtn body listener to index

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,13 @@
-window.addEventListener('scroll', function() {
-    console.log('scrolling');
-});
+document.body.addEventListener('scroll', function(){
+    const scrolled = document.body.scrollTop;
+    const height = document.body.clientHeight;
+    const cutoff = height / 3.5;
+
+    if(scrolled > cutoff){
+        $('.header').css("background-color", "#172b1d");
+    }
+    else{
+        $('.header').css("background-color", "rgba(0, 0, 0, 0)");
+
+    }
+})
